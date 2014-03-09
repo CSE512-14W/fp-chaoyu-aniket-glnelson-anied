657a23828e1cc8dc0abaa30915977d32cf11b498
paranoyang <paranoyang@gmail.com>
2014-03-09 05:34:55 -0700

    formatting data

--- src/flow.js
+++ src/flow.js
@@ -21,46 +21,60 @@
  */
 
 
-(function(){
-  /* Generating flow object
-   */
-  d3.flow = function() {
-      var flow = {},
-          flow.data = {},
-          flow.nodes = [],
-          flow.links = [],
-          flow.duration = 1000,
-          flow.currnt = 0;
+(function(_){
+  var flow = function() {
+    var flow = {};
+    flow.data = {},
+    flow.nodes = [],
+    flow.links = [],
+    flow.duration = 1000;
 
+    /* 
+     * flow.node(nodes)
+     *     .links(links)
+     *     .data(data)
+     *     .init(svg);
+     */
+    flow.init = function(svg) {
+      flow.period = data.length;
+      flow.cur = 0;
+      flow.timeline = _.keys(data); 
+      flow.packet = svg.selectAll('.packet');
+    }
+
+
+    /* d:
+     *   [{src, snk, value},...]
+     *   animations in one time unit
+     */
+    flow.tick = function() {
+      flow.packet.data(flow.data[timeline[cur%flow.period]])
       
 
-      /* d:
-       *   [{src, snk, value},...]
-       *   animations in one time unit
-       */
-      flow.tick = function(d) {
-        
-      
-      };
+    };
 
-      flow.loop = function() {
-      
-      };
+    flow.loop = function() {
 
-      flow.nodes = function(_) {
-        if (!arguments.length) return nodes;
-        nodes = _;
-        return flow;
-      };
+    };
 
-      flow.links = function(_) {
-        if (!arguments.length) return links;
-        links = _;
-        return flow;
-      };
-
-
+    flow.data = function(_) {
+      if (!arguments.length) return data;
+      data = _;
       return flow;
-  };
+    };
 
-})();
+    flow.nodes = function(_) {
+      if (!arguments.length) return nodes;
+      nodes = _;
+      return flow;
+    };
+
+    flow.links = function(_) {
+      if (!arguments.length) return links;
+      links = _;
+      return flow;
+    };
+
+    return flow;
+  }; // end of d3.flow=
+})(_);
