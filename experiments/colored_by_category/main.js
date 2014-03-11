9456aca98d5cefb2d518d8953e3af1a212af7380
paranoyang <paranoyang@gmail.com>
2014-03-10 19:51:11 -0700

    colored by category experiment

--- experiments/colored_by_category/main.js
+++ experiments/colored_by_category/main.js
@@ -28,14 +28,18 @@
                     .range([0 + margin.top, height - margin.bottom])
                     .nice();
 
+    var categories = _.union(_.map(dataset, function(x){ return x.label; }));
+    var c_scale = d3.scale.category20().domain(categories);
+
     node = node.data(dataset)
         .enter()
         .append("circle")
         .attr({
           "class": "node",
+          "fill": function(d){ return c_scale(d.label); },
           "r": 3,
-          "cx": function(d){ return x_scale(d.x)},
-          "cy": function(d){ return y_scale(d.y)}
+          "cx": function(d){ return x_scale(d.x); },
+          "cy": function(d){ return y_scale(d.y); }
         });
 
     d3.csv("../../data/F_PTC3_words_LD_E.csv", function(data) {
@@ -70,6 +74,7 @@
           .enter()
           .append("circle")
           .attr("class", '.packet')
+          .attr("fill", function(d){ return c_scale(node.data()[d.source -1].label); })
           .attr("r", 2)
           .attr("cx", function(d){ return x_scale(node.data()[d.source - 1].x); })
           .attr("cy", function(d){ return y_scale(node.data()[d.source - 1].y); })
@@ -95,6 +100,9 @@
         area: d.area,
         plot: d.plot
       };
+    });
+    _.each(dataset, function(d){ 
+      d.label = d.label.slice(0, d.label.indexOf("_"));
     });
     ptc3_network(dataset);
   });
